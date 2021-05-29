a6ea1a82a30784bbff23f4c0442562cce7eb1b9b
Nujood <shawar.nujood@gmail.com>
2021-05-30 00:05:30 +0300

    Establish a connection on MongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ItemsController } from './items/items.controller';
+import { itemsModule } from './items/items.module';
 import { ItemsService } from './items/items.service';
+import config from './config/keys';
 
 @Module({
-  imports: [],
+  imports: [itemsModule, MongooseModule.forRoot(config.mongoURL)],
   controllers: [AppController, ItemsController],
   providers: [AppService, ItemsService],
 })
