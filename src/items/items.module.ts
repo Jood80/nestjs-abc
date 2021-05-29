b7621d736c6a382dc38ca34e8dea6f23db16adea
Nujood <shawar.nujood@gmail.com>
2021-05-30 00:29:23 +0300

    Add mongo schema into the items.module

--- src/items/items.module.ts
+++ src/items/items.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ItemSchema } from 'src/schemas/item.schema';
 import { ItemsController } from './items.controller';
 import { ItemsService } from './items.service';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forFeature([{ name: 'item', schema: ItemSchema }])],
   controllers: [ItemsController],
   providers: [ItemsService],
 })
