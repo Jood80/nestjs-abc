9debb23260c5428efd0418c7c972b2e7e55d94c0
Nujood <shawar.nujood@gmail.com>
2021-05-29 22:42:04 +0300

    Done with items CRUD

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,16 +1,47 @@
-import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Header,
+  HttpCode,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { CreateItemDto } from './dto/create-items.dto';
-import { Request, Response } from 'express';
+
 @Controller('items')
 export class ItemsController {
   @Get()
-  findAll(@Req() req: Request, @Res() res: Response): Response {
-    console.log(req.url);
-    return res.send('Hello from Express');
+  findAll(): string {
+    return 'Get All items';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id) {
+    return `This is item No.${id}`;
   }
 
   @Post()
+  @HttpCode(204)
+  @Header('Cache-Control', 'none')
   async create(@Body() createItemDto: CreateItemDto) {
-    return ` Name:${createItemDto.name}\n Quantity: ${createItemDto.quantity}\n Description: ${createItemDto.desc}`;
+    return `Name:${createItemDto.name}
+    Quantity: ${createItemDto.quantity}
+    Description: ${createItemDto.desc}`;
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id): string {
+    return `Delete item No.${id}`;
+  }
+
+  @Put(':id')
+  update(@Body() updateItemDto: CreateItemDto, @Param('id') id): string {
+    return `Update item No.${id}:
+    Name:${updateItemDto.name}
+    Quantity: ${updateItemDto.quantity}
+    Description: ${updateItemDto.desc}`;
   }
 }
