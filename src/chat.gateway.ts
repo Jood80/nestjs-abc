24f16f569e86bcd3ae7b594e45cf98988a742b5e
Nujood <shawar.nujood@gmail.com>
2021-06-01 23:41:04 +0300

    Adjust the namespace fro websocketGateway

--- src/chat.gateway.ts
+++ src/chat.gateway.ts
@@ -1,17 +1,27 @@
 import {
   MessageBody,
+  OnGatewayInit,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
 
-@WebSocketGateway()
-export class ChatGateway {
+@WebSocketGateway(3001, {
+  namespace: '/chatting',
+  secure: true,
+  rejecetUnauthorized: true,
+})
+export class ChatGateway implements OnGatewayInit {
   @WebSocketServer()
   server;
 
+  afterInit(server: any) {
+    console.log('Initialized');
+  }
+
   @SubscribeMessage('message')
-  handleMessage(@MessageBody() message: string): void {
+  handleMessage(@MessageBody() message: string): string {
     this.server.emit('message', message);
+    return 'Hello worldssss';
   }
 }
