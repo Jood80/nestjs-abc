cc5a52c0898834424e1e1740c68517c8e788f313
Nujood <shawar.nujood@gmail.com>
2021-06-02 23:47:51 +0300

    Documenting the Urls using Swagger API

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 
@@ -8,6 +9,17 @@ const port = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+
+  const options = new DocumentBuilder()
+    .setTitle('NestJs ABC API')
+    .setDescription('Nothing Fancy')
+    .setVersion('4.8.0')
+    .build();
+
+  const apiDocument = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, apiDocument);
+
   await app.listen(port);
   Logger.log(`Server's running on http://loalhost:${port}`, 'Bootstrap');
 }
