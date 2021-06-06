8c84b6d1e833b1f2dbaf3019be72a660dd618a2f
Nujood <shawar.nujood@gmail.com>
2021-06-06 18:52:24 +0300

    Fixing the unpassed tests

--- src/items/items.service.spec.ts
+++ src/items/items.service.spec.ts
@@ -1,18 +1,37 @@
+import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { ItemsController } from './items.controller';
 import { ItemsService } from './items.service';
 
 describe('ItemsService', () => {
-  let service: ItemsService;
+  let service: INestApplication;
+  // let service: ItemsService;
+  let controller: ItemsController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ItemsService],
+      controllers: [ItemsController],
+      providers: [
+        {
+          provide: ItemsService,
+          useValue: {
+            findAll: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
+    controller = module.get<ItemsController>(ItemsController);
     service = module.get<ItemsService>(ItemsService);
+    // service = module.createNestApplication();
+    // await service.init()
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  // afterAll(async () => {
+  //   await service.close();
+  // });
 });
